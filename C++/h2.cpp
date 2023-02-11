a4d9a29dc42967aac7406f7d02b8b19da05d68da
rzeta-10 <125035257+rzeta-10@users.noreply.github.com>
2023-02-11 16:28:19 +0530

    Update h2.cpp

--- C++/h2.cpp
+++ C++/h2.cpp
@@ -18,7 +18,7 @@ int count(int coins[], int n, int sum)
 	for (i = 1; i < sum + 1; i++) {
 		for (j = 0; j < n; j++) {
 
-			x = (i - coins[j] >= 0) ? table[j][i - coins[j]] : 0;
+			x = (i - coins[j] >= 0) ? table[j][i - coins[j]] : 0;  //replaced 1 by 0 in ternary condition
 
 
 			y = (j > 1) ? table[j - 1][i] : 1;
