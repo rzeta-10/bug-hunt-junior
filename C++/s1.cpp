4bb02e3c5cf3b93ad5fbbd347f7784ce8ff8af26
rzeta-10 <125035257+rzeta-10@users.noreply.github.com>
2023-02-11 16:26:32 +0530

    Update s1.cpp

--- C++/s1.cpp
+++ C++/s1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int reverse(int x) {
     long r=0;                                  
     while(x){
-        r=r*10+x/10;
+        r=r*10+x%10;
         x=x/10;                                   
     }
     return r;
